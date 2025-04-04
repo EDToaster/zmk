d60667b6c51c13d2593227941e43bd58b6df1e6a
EDToaster <howard3061@gmail.com>
2025-04-04 09:22:00 -0700

    oops2

--- app/boards/shields/nice_view/widgets/art.c
+++ app/boards/shields/nice_view/widgets/art.c
@@ -17,7 +17,8 @@
 #endif
 
 const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_FRAME1 uint8_t
-    frame1_map[] = {#if CONFIG_NICE_VIEW_WIDGET_INVERTED
+    frame1_map[] = {
+#if CONFIG_NICE_VIEW_WIDGET_INVERTED
         0xff, 0xff, 0xff, 0xff, /*Color of index 0*/
         0x00, 0x00, 0x00, 0xff, /*Color of index 1*/
 #else
